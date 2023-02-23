7821adc94e62f831132da3ad671068ff6075e5fd
Brais Solla <brais.1997@gmail.com>
2023-02-23 17:53:32 +0100

    Creada imagen propia

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-def main():
-    print ("Hola PyTube")
+from pytube import YouTube
 
-if __name__ == "__main__":
-    main()
+yt = YouTube("https://www.youtube.com/watch?v=hqRFWCbQ9Lk")
+
+#Title of video
+print("Title: ",yt.title)
+#Number of views of video
+print("Number of views: ",yt.views)
+#Length of the video
+print("Length of video: ",yt.length,"seconds")
+#Description of video
+print("Description: ",yt.description)
+#Rating
+print("Ratings: ",yt.rating)
+
+# yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
